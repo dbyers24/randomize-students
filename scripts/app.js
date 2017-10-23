c74a64fe2db697a2959673dee3db693545ae9267
Dustin Byers <dbyers84@gmail.com>
2017-10-23 10:57:56 -0700

    figured

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,37 @@
 'use strict';
 
-// console.log( 'xmlhttp :', xmlhttp );
-// console.log( 'xmlhttp property: ' + xmlhttp.abort );
-// console.log( xmlhttp.length );
+var fs = require('fs')
 
-// var main = document.getElementById( 'main' );
-// var form = document.createElement('form');
-// var input = document.createElement('input')
-// main.appendChild(form)
-// form.appendChild(input);
+{/* <button class='add-student-button' type='submit'>  +  </button?
+          <label># of groups
+          <input id='number-groups-input' type='number' />
+          </label>
+          <label>Group Size: 
+          <input id='size-groups-input' type='number' />
+          </label>
+      <button class='submit-button' type='submit'> SUBMIT </button> */}
 
+let addStudentButton = document.getElementById('add-student-button')
+let submitButton = document.getElementById('submit-button')
+let numberGroupsInput = document.getElementById('number-groups-input')
+let addStudentInput = document.getElementById('add-student-input')
+let sizeGroupsInput = document.getElementById('size-groups-input')
+let output = document.getElementById('output-box')
 
-console.log( main );
+let handleAddStudent = (e) => {
+  console.log('HIT handleAddStudent: ', addStudentInput.value)
+
+  fs.write('students.txt', addStudentInput.value, () => {
+    console.log('damn son, that shit worked.')
+  })
+
+  output.value += addStudentInput.value
+  addStudentInput.value = null;
+}
+
+let handleSubmit = (e) => {
+  event.preventDefault();
+  console.log('HIT handleSubmit: ', '# grps: ', numberGroupsInput.value, 'grp size: ', sizeGroupsInput.value,  )  
+}
+addStudentButton.addEventListener('click', handleAddStudent)
+submitButton.addEventListener('click', handleSubmit)
